2dce696c7670ffa9d17820ef985f99659d7d421f
Aniruddha Roy Arka <aniruddharoyarka2@gmail.com>
2026-08-29 22:01:17 +0600

    updated git ignore

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
-        console.log("MongoDB connected");
+        console.log("MongoDB connectted");
     })
     .catch((error) => {
         console.log("MongoDB connection failed:", error);
